6ee349d27cc76858bea1889bb568729f26ad29d9
Amanda Ekaratih Siswojo <aes793@nyu.edu>
2018-03-02 00:28:56 -0500

    Platformer game, but level editor is still not working

---
+++ Assets/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    public GameObject playerPrefab;
+    public GameObject groundPrefab;
+    public GameObject enemyPrefab;
+    public GameObject pigPrefab;
+
+    public float tileWidth = 0.1f;
+    public float tileHeight = 0.1f;
+
+    public string levelFile = "levelfile.txt";
+
+
+	// Use this for initialization
+	void Start () {
+		
+        string levelString = File.ReadAllText(Application.dataPath + Path.DirectorySeparatorChar + levelFile);
+        string[] levelLines = levelString.Split('\n');
+        for(int row = 0; row < levelLines.Length; row++)
+        {
+            string currentLine = levelLines[row];
+            tileWidth = currentLine.Length;
+            for (int col = 0; col < currentLine.Length; col++)
+            {
+                char currentChar = currentLine[col];
+                if (currentChar == 'x')
+                {
+                    GameObject groundObj = Instantiate(groundPrefab);
+                    groundObj.transform.position = new Vector3(col * tileWidth, -row * tileHeight, 0);
+                }
+                else if (currentChar == 'p')
+                {
+                    GameObject playerObj = Instantiate(playerPrefab);
+                    playerObj.transform.position = new Vector3(col * tileWidth, -row * tileHeight, 0);
+                }
+                else if (currentChar == 'e')
+                {
+                    GameObject enemyObj = Instantiate(enemyPrefab);
+                    enemyObj.transform.position = new Vector3(col * tileWidth, -row * tileHeight, 0);
+                }
+                else if (currentChar == 't')
+                {
+                    GameObject pigObj = Instantiate(pigPrefab);
+                    pigObj.transform.position = new Vector3(col * tileWidth, -row * tileHeight, 0);
+                }
+
+            }
+        }
+
+        //float myX = -(width*tileWidth) / 2f + tileWidth / 2f;
+        //float myY = (levelLines.Length * tileHeight) / 2f - tileHeight / 2f;
+        //transform.position = new Vector3(myX, myY, 0);
+    }
+   
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    float velX;
+    float velY;
+
+    public float moveSpeed = 10f;
+    public bool facingRight = true;
+    Rigidbody2D rb;
+    
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        velX = Input.GetAxisRaw("Horizontal");
+        velY = rb.velocity.y;
+        rb.velocity = new Vector2(velX * moveSpeed, velY);
+
+    }
+
+    void LateUpdate()
+    {
+        Vector3 localScale = transform.localScale;
+        if (velX > 0)
+        {
+            facingRight = true;
+        }
+        else if (velX < 0) 
+        {
+            facingRight = false;
+        }
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+        {
+            localScale.x *= -1;
+        }
+
+        transform.localScale = localScale;
+    }
+}
